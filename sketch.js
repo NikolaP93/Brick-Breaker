5f727ebb0f45acd55f0c597df38f1fcbddc0b6f7
Nikola Prijovic <nikola.prijovic1@gmail.com>
2019-08-07 19:24:12 +0200

    -Introduced sound, major bugfix, code cleanup
    - Added a sound feature when the ball hits a brick
    - Correct amount of bricks now displaying
    - Removed phantom brick bug due to incorrect number of bricks showing
    - Ball speed now properly scales with the score
    
    *Note , switching to OOP , implementing more ES6 features in the future*

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 
-let brick;
+let brick, status, score, song;
 let bricks = [];
-let status;
-let score;
 let gameStatus = true;
-let lives = 3;
-let counter = 0;
 
+let currentScore = 0;
+
+let initializers = {
+    lives:3,
+    gameStatus:true,
+    currentScore:0
+}
 
 function mousePressed() {
     if (gameStatus) {
@@ -16,45 +19,17 @@ function mousePressed() {
     }
 }
 
-function removeBricks() {
-    
-    
-    
-    for (i = 0; i < 12; i++) {
-        for (j = 0; j < bricks[i].length; j++) {
 
+function init() {
 
-            // remove bricks
-            
-            if (ball.hits(bricks[i][j])) {
-                bricks[i].splice(j, 1);
-                score++;
-                ball.yspeed = 3;
-
-                //accelerate the ball on impact
-                if (ball.xspeed > 0) {
-                    ball.xspeed = 3 + score / 10
-                }
-                if (ball.xspeed < 0) {
-                    ball.xspeed = -3 - score / 10;
-                }
-            }
-        }
-    }
-}
-
-function newGame() {
-    
-   
     status = new GameStatus();
-
+    highScore = 0;
     ball = new Ball();
-    
     img = loadImage('images/brick.jpg');
     livesimg = loadImage('images/heart.png');
     backgroundimg = loadImage('images/spacebgr.jpg');
 
-    if (lives === 3) {
+    if (initializers.lives === 3) {
         ball = new Ball();
         score = 0;
         brick = new Brick(width - (width * 7 / 100), height - (height * 10 / 100), height / 80, width / 12);
@@ -63,28 +38,57 @@ function newGame() {
             bricks[i] = [];
 
             for (j = 0; j < 6; j++) {
-                bricks[i][j] = new Brick(5 + i * (width * 1 / 10), 10 + j * 30, 20, width / 12);
+
+                bricks[i][j] = new Brick(5 + i * (width / 12), 10 + j * (height / 20), height / 30, width / 14)
+
             }
         }
-
-            
     }
-
-    removeBricks();
-
-    
 
 }
 
+function removeBricks() {
+    for (i = 0; i < 12; i++) {
+
+        for (j = bricks[i].length - 1; j >= 0; j--) {
+            bricks[i][j].show();
+
+            // remove bricks
+            if (ball.hits(bricks[i][j])) {
+                currentScore++;
+                song.play();
+
+                bricks[i].splice(j, 1);
+                
+                score++;
+                ball.yspeed = 3;
+
+                //accelerate the ball
+                if (ball.xspeed > 0) {
+
+                    ball.xspeed = 3 + currentScore/10;
+                    
+                }
+                if (ball.xspeed < 0) {
+                    ball.xspeed = -3 - currentScore/10
+                }
+
+            }
+        }
+    }
+}
+
+
 function setup() {
-    createCanvas(800, 600);
-    newGame();
+    createCanvas(1200, 800);
+    init();
+    song = loadSound('tick.mp3');
 }
 
 
 
 function draw() {
-    counter++;
+    
     background(backgroundimg);
     fill(255);
 
@@ -93,64 +97,39 @@ function draw() {
 
     ball.show();
     ball.move();
-    
-    // game status / lives counter
+
+    // game status / life counter
     if (ball.y > height + 50) {
-        --lives;
+        --initializers.lives;
+        currentScore = 0;
         gameStatus = true;
-        newGame();
+        init();
         
 
-        if (lives < 1) {
+
+        if (initializers.lives < 1) {
             if (confirm("Play again?")) {
-                lives = 3;
-                newGame();
-                
+                initializers.lives = 3;
+                init();
+
             }
         }
     }
+
     //display lives
-    status.displayLives(lives);
+    status.displayLives(initializers.lives);
     //display score
     status.drawScore();
 
 
-
+    //display bricks
     if (ball.hits(brick)) {
         ball.yspeed = -3;
     }
-
-    // display the bricks
-    for (i = 0; i < 12; i++) {
-        
-        for (j = bricks[i].length - 1; j >= 0; j--) {
-            bricks[i][j].show();
-
-            // remove bricks
-            if (ball.hits(bricks[i][j])) {
-             
-                
-                    if(counter>30) {
-                        bricks[i].splice(j, 1);
-                
-                    }
-                    
-                
+    removeBricks(currentScore);
 
 
-                score++;
-                ball.yspeed = 3;
 
-                //accelerate the ball
-                if (ball.xspeed > 0) {
-                    ball.xspeed = 3 + score / 10
-                }
-                if (ball.xspeed < 0) {
-                    ball.xspeed = -3 - score / 10;
-                }
-            }
-        }
-    }
 
 
 }
